64a1ae8bf96450b1b2f31e2e54ddf95b12d002bb
AutomaticcData <41269362+AutomaticcData@users.noreply.github.com>
2018-08-10 13:55:47 -0700

    Add files via upload
    # Instructions
    
    * Read through `cereal.csv` and find the cereals that contain five grams of fiber or more, printing the data from those rows to the terminal.
    
    ## Hint
    
    * Everything within the csv is stored as a string and certain rows have a decimal. This means that they will have to be cast to be used.
    
    * The csv.Reader begins reading the csv file at the first row. Explain that `next(csv_reader, None)` will skip the header row.
    
    * Integers in Python are whole numbers and, as such, cannot contain decimals. As such, your numbers containing decimal points will have to be cast as a `float` or `double`.
    
    ## Bonus
    
    * Try the following again but this time using `cereal_bonus.csv`, which does not include a header.

---
+++ Python_Week1_03/03 Cereal/cereal_aalvarez.py
@@ -0,0 +1,56 @@
+"""
+Created on Thu Aug  10 1:35:19 2018
+
+
+@author: aalvarez
+
+References: in code below
+
+
+Instructions
+Read through cereal.csv and find the cereals that contain five grams of fiber or more, printing the data from those rows to the terminal.
+Hint
+Everything within the csv is stored as a string and certain rows have a decimal. This means that they will have to be cast to be used.
+The csv.Reader begins reading the csv file at the first row. Explain that next(csv_reader, None) will skip the header row.
+Integers in Python are whole numbers and, as such, cannot contain decimals. As such, your numbers containing decimal points will have to be cast as a float or double.
+
+Bonus
+Try the following again but this time using cereal_bonus.csv, which does not include a header.
+
+import os
+import csv
+cereal_csv = os.path.join("../Resources", "cereal.csv")
+"""
+
+#import the os Module
+import os
+
+#import the csv csvreader
+import csv
+
+#csv is currently in the same directory as the python script, using blank parameters as such
+csv_path = os.path.join('','','cereal.csv')
+
+#open the file, using the delimiter as the csv file
+#another UnicodeDecodeError, adding encoding parameter to solve this jibberish 'ï»¿name'
+#unfortunately utf8 did not work and so research ensued
+#https://stackoverflow.com/questions/17912307/u-ufeff-in-python-string
+with open(csv_path, newline='', encoding='utf-8-sig') as csv_file:
+
+    #assign the contents of the file to a variable called csvreader
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    #assign the header to the csv_header variable, we are using this to skip the entire
+
+    #first row which contains the header
+    csv_header = next(csv_reader)
+    #print(f"CSV Header: {csv_header}")
+
+    for row in csv_reader:
+        #print(row)
+        #open the file and see which column contains the fiber values
+        #convert col values to Integers
+        #https://stackoverflow.com/questions/1094717/convert-a-string-to-integer-with-decimal-in-python
+        num_decimal = int(float(str(row[7])))
+        if num_decimal>= 5:
+            print(num_decimal)
